65943f691f56a540a9748180c8427d114e48dd45
Yuting <leyuting0424@outlook.com>
2020-04-19 20:38:53 -0400

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const { data } = require('./data.json');
+const { projects } = require('./data.json');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-app.get('/', (req, res, next)=>{
-    res.locals = data.projects;
-    res.render('index', { data });
+app.get('/', (req, res)=>{
+    //res.locals = data.projects;
+    res.render('index', { projects });
 });
 
-app.get('/about', (req, res, next)=>{
-    res.render('about', { data });
+app.get('/about', (req, res)=>{
+    res.render('about', { projects });
 })
 
-//error handler
+app.get('/project/:id', (req, res)=>{
+    const projectId = req.params.id;
+    const project = projects.find( ({id}) => id === +projectId);
+    
+    if(project){
+        res.render('project', {project});
+    } else {
+        res.sendStatus(404);
+    }
+});
 
+//error handler
+app.use((err, req, res, next)=>{
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+})
 
 app.listen(3000, ()=>{
     console.log("The application is running on localhost: 3000.")
