98865edb0370d4afcc61710f61d7d93915bb6fad
Yuting <leyuting0424@outlook.com>
2020-04-20 12:10:58 -0400

    added error handlers and made style changes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res)=>{
-    //res.locals = data.projects;
     res.render('index', { projects });
 });
 
@@ -27,11 +26,16 @@ app.get('/project/:id', (req, res)=>{
 });
 
 //error handler
+app.use((req, res, next)=>{
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+});
 app.use((err, req, res, next)=>{
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
-})
+});
 
 app.listen(3000, ()=>{
     console.log("The application is running on localhost: 3000.")
